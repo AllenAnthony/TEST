46e907cec88ccebfcc8c250978794837a85bbd63
Anthony <1875898003@qq.com>
2017-05-23 16:03:19 +0800

    '

--- try.cpp
+++ try.cpp
@@ -4,21 +4,6 @@ using namespace std;
 
 int main()
 {
-    int zxcv;
-    int bbb;
-
-    //qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-    //strat to change
-    double dsfa;
-    int aaa;
-    for(int i=1;i<10;i++);
-    // dfsaeafw
-    // ewafrgfqrg
-    //asdffdsa
     int a;
-<<<<<<< Updated upstream
-    gdfgs
-=======
-    //mmmmmmmmmmmmmmmmmmmmmmmmmmmmm
->>>>>>> Stashed changes
+    int b;
 }
